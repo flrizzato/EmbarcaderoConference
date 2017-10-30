5b54ebd12ca1334a20400a985a4e1f45f088cb04
flrizzato <fernando.rizzato@gmail.com>
2017-10-30 10:50:33 -0200

    EC 2017 v5

---
+++ 2017/Samples/IOTWEB - Arduino DataSnap IoT Made in Delphi/ProjetoDataSnap/ServerMethodsUnit1.pas
@@ -0,0 +1,38 @@
+unit ServerMethodsUnit1;
+
+interface
+
+uses System.SysUtils, System.Classes, System.Json,
+    DataSnap.DSProviderDataModuleAdapter,
+    Datasnap.DSServer, Datasnap.DSAuth;
+
+type
+  TServerMethods1 = class(TDSServerModule)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+    function EchoString(Value: string): string;
+    function ReverseString(Value: string): string;
+  end;
+
+implementation
+
+
+{$R *.dfm}
+
+
+uses System.StrUtils;
+
+function TServerMethods1.EchoString(Value: string): string;
+begin
+  Result := Value;
+end;
+
+function TServerMethods1.ReverseString(Value: string): string;
+begin
+  Result := System.StrUtils.ReverseString(Value);
+end;
+
+end.
+
